9ad094b12e943e21fec585d709e5c64e086ccbe3
hagawaga <bjoern.andre.woltering@gmail.com>
2018-05-02 14:05:05 +0200

    1

---
+++ FahrAway/Drop-fahraway.sql
@@ -0,0 +1,30 @@
+DROP TABLE schueler ;
+
+DROP TABLE fahrt ;
+
+DROP TABLE unterkunft ;
+
+DROP TABLE ort ; 
+
+DROP TABLE lehrer ;
+
+DROP TABLE aktivitaet ;
+
+DROP TABLE anbieter ;
+
+DROP TABLE bietetan ;
+
+DROP TABLE wirdangeboten ;
+
+DROP TABLE bezieht ;
+
+DROP TABLE istin ;
+
+DROP TABLE nimmtteil;
+
+DROP TABLE begleitet ;
+
+
+
+
+		
